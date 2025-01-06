166d44637833bc58490b2ebe25d6667fe4f81aae
Ethan L. Mines <elmines@users.noreply.github.com>
2025-01-06 16:42:31 -0500

    Add simple limit_n utility

--- gatbert/utils.py
+++ gatbert/utils.py
@@ -4,6 +4,12 @@ from typing import List, Any
 def flat_map(f, iterable) -> List[Any]:
     return reduce(lambda a,b: a + b, map(f, iterable), [])
 
+def limit_n(iterable, n):
+    for (i, el) in enumerate(iterable, start=1):
+        yield el
+        if i == n:
+            break
+
 def batched(iterable, n: int):
     batch = []
     for el in iterable:
