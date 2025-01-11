7e7a156e5225bd266c2369387dd6d8fe1db35d74
Ethan L. Mines <elmines@users.noreply.github.com>
2025-01-11 17:06:54 -0500

    WIP: Extract CN graphs

---
+++ dbops.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python3
+
+# STL
+import argparse
+import pdb
+import sys
+import dataclasses
+import functools
+# 3rd Party
+import psycopg2
+import psycopg2.extras
+from tokenizer.pre_tokenizers import BertPreTokenizer
+# local
+from gatbert.data import Sample, parse_ez_stance
+from gatbert.constants import CN_RELATIONS, REV_RELATIONS
+
+# Assumes a concept5 postgres DB fully populated according to these instructions: https://github.com/commonsense/conceptnet5/wiki/Build-process
+
+conn = psycopg2.connect("dbname='conceptnet5'")
+conn.autocommit = False
+print(conn.autocommit, conn.isolation_level)
+
+sys.exit(1)
+
+def generate_degree_column(conn):
+    with conn:
+        with conn.cursor() as curs:
+            try:
+                curs.execute("ALTER TABLE nodes DROP COLUMN IF EXISTS degree")
+                curs.execute("ALTER TABLE nodes DROP COLUMN IF EXISTS out_degree")
+                curs.execute("ALTER TABLE nodes DROP COLUMN IF EXISTS in_degree")
+                curs.execute("ALTER TABLE nodes ADD COLUMN out_degree INTEGER DEFAULT 0")
+                curs.execute("ALTER TABLE nodes ADD COLUMN  in_degree INTEGER DEFAULT 0")
+                curs.execute("""update nodes n
+                                set out_degree = subquery.out_degree
+                                from (
+                                    select start_id as id,
+                                            count(*) as out_degree
+                                    from edges group by id
+                                ) as subquery
+                                where n.id = subquery.id;""")
+                curs.execute("ALTER TABLE nodes ADD COLUMN degree INTEGER GENERATED ALWAYS AS in_degree+out_degree")
+            except (Exception, psycopg2.DatabaseError) as err:
+                conn.rollback()
+                print(err)
+
+@dataclasses.dataclass
+class GraphSample:
+    pass
+
+def extract(conn, sample_gen, N=2):
+
+    pretok = BertPreTokenizer()
+
+    cursor = conn.cursor('cursor_unique_name', psycopg2.extras.DictCursor)
+
+    @functools.lru_cache
+    def find_match(token):
+        token = token.lower()
+        cursor.execute("SELECT id,uri FROM nodes ORDER BY degree DESC LIMIT 1")
+        records = cursor.fetchall()
+        if records:
+            return (records[0]['id'], records[0]['uri'])
+        return None
+
+    @functools.lru_cache
+    def find_neighbors(node_id):
+        cursor.execute("SELECT start_id,end_id,relation_id FROM edges WHERE start_id=node_id OR end_id=node_id")
+        records = cursor.fetchall()
+
+        neighbors = set()
+        edges = {}
+        
+        for rec in records:
+            orig_id = rec['relation_id']
+            start_id = rec['start_id']
+            end_id = rec['end_id']
+            if start_id != node_id:
+                neighbors.add(start_id)
+            elif end_id != node_id:
+                neighbors.add(end_id)
+
+            rel_obj = CN_RELATIONS[orig_id]
+            edges.append( (start_id, end_id, rel_obj.internal_id) )
+
+            rev_rel_obj = REV_RELATIONS.get(orig_id)
+            if rev_rel_obj:
+                edges.append( (end_id, start_id, rev_rel_obj.internal_id) )
+
+        return neighbors, edges
+
+    for sample in sample_gen:
+        context = sample.context
+        target = sample.target
+        stance = sample.stance.value
+
+        target_toks = pretok.pre_tokenize_str(target)
+        N_target = len(target_toks)
+        context_toks = pretok.pre_tokenize_str(context)
+        N_context = len(context_toks)
+        toks = target_toks + context_toks
+
+        for tok in toks:
+            match = find_match(tok)
+            if not match:
+                continue
+            (node_id, node_uri) = match
+
+            pass
+
+        yield [stance, N_target, N_context, *toks]
+
+
+def main(raw_args=None):
+    parser = argparse.ArgumentParser(description='Extract subgraphs from ConceptNet for stance samples')
+
+    parser.add_argument("--gen_degree", action="store_true", help="Force regeneration of the degree column for the nodes table")
+
+    parser.add_argument("--ezstance", type=str, required=True, metavar="input.csv", help="File containing stance data from the EZStance dataset")
+    parser.add_argument("--vast",     type=str, required=True, metavar="input.csv", help="File containing stance data from the VAST dataset")
+    parser.add_argument("--semeval",  type=str, required=True, metavar="input.txt", help="File containing stance data from SemEval2016-Task6")
+    parser.add_argument("-o",         type=str, required=True, metavar="output_file.tsv", help="File pretokenized stance samples, with graph nodes")
+    args = parser.parse_arg(raw_args)
+
+    if not any([args.ezstance, args.vast, args.semeval]):
+        print("Must select one of --ezstance, --vast, or --semeval", file=sys.stderr)
+        sys.exit(1)
+    if args.ezstance:
+        sample_gen = parse_ezstance(args.ezstance)
+    elif args.vast:
+        raise RuntimeError("--vast not yet supported")
+    else:
+        raise RuntimeError("--semeval not yet supported")
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
--- gatbert/constants.py
+++ gatbert/constants.py
@@ -1,4 +1,5 @@
 import enum
+import dataclasses
 
 @enum.unique
 class SemEvalTargets(enum.Enum):
@@ -55,3 +56,77 @@ NUM_FAKE_NODES = 1000
 """
 Used for dummy routines testing graph logic
 """
+
+@dataclasses.dataclass
+class Relation:
+    orig_id: int
+    internal_id: int
+    name: str
+    directed: bool
+
+__RAW_RELATIONS = [
+    ( 0, "/r/Antonym",                   False),
+    ( 1, "/r/AtLocation",                True) ,
+    ( 2, "/r/CapableOf",                 True) ,
+    ( 3, "/r/Causes",                    True) ,
+    ( 4, "/r/CausesDesire",              True) ,
+    ( 5, "/r/CreatedBy",                 True) ,
+    ( 6, "/r/DefinedAs",                 True) ,
+    ( 7, "/r/DerivedFrom",               True) ,
+    ( 8, "/r/Desires",                   True) ,
+    ( 9, "/r/DistinctFrom",              False),
+    (10, "/r/Entails",                   True) ,
+    (11, "/r/EtymologicallyDerivedFrom", True) ,
+    (12, "/r/EtymologicallyRelatedTo",   False),
+    (13, "/r/ExternalURL",               True) ,
+    (14, "/r/FormOf",                    True) ,
+    (15, "/r/HasA",                      True) ,
+    (16, "/r/HasContext",                True) ,
+    (17, "/r/HasFirstSubevent",          True) ,
+    (18, "/r/HasLastSubevent",           True) ,
+    (19, "/r/HasPrerequisite",           True) ,
+    (20, "/r/HasProperty",               True) ,
+    (21, "/r/HasSubevent",               True) ,
+    (22, "/r/InstanceOf",                True) ,
+    (23, "/r/IsA",                       True) ,
+    (24, "/r/LocatedNear",               True) ,
+    (25, "/r/MadeOf",                    True) ,
+    (26, "/r/MannerOf",                  True) ,
+    (27, "/r/MotivatedByGoal",           True) ,
+    (28, "/r/NotCapableOf",              True) ,
+    (29, "/r/NotDesires",                True) ,
+    (30, "/r/NotHasProperty",            True) ,
+    (31, "/r/NotUsedFor",                True) ,
+    (32, "/r/ObstructedBy",              True) ,
+    (33, "/r/PartOf",                    True) ,
+    (34, "/r/ReceivesAction",            True) ,
+    (35, "/r/RelatedTo",                 False),
+    (36, "/r/SimilarTo",                 False),
+    (37, "/r/SymbolOf",                  True) ,
+    (38, "/r/Synonym",                   False),
+    (39, "/r/UsedFor",                   True) ,
+    (40, "/r/dbpedia/capital",           True) ,
+    (41, "/r/dbpedia/field",             True) ,
+    (42, "/r/dbpedia/genre",             True) ,
+    (43, "/r/dbpedia/genus",             True) ,
+    (44, "/r/dbpedia/influencedBy",      True) ,
+    (45, "/r/dbpedia/knownFor",          True) ,
+    (46, "/r/dbpedia/language",          True) ,
+    (47, "/r/dbpedia/leader",            True) ,
+    (48, "/r/dbpedia/occupation",        True) ,
+    (49, "/r/dbpedia/product",           True) ,
+]
+
+PADDING_RELATION_ID = 0
+TOKEN_RELATION_ID = 1
+
+CN_RELATIONS: Dict[int, Relation] = {
+        orig_id: Relation(orig_id, internal_id, name, directed) 
+        for (internal_id, (orig_id, name, directed)) in enumerate(__RAW_RELATIONS, start=2)
+}
+
+REV_RELATIONS: Dict[int, Relation] = {
+        orig_id: Relation(orig_id, internal_id, f"/reverse/{name}", directed) 
+        for (internal_id, (orig_id, name, directed)) in enumerate(filter(lambda r: r[2], __RAW_RELATIONS), start=len(RELATIONS)+3)
+}
+
