cb53f05c0c2004ec9a8bd8f19ea3db0992203033
Ethan L. Mines <elmines@users.noreply.github.com>
2025-03-29 15:51:21 -0400

    Make bert classifier a lightning module

--- gatbert/modules.py
+++ gatbert/modules.py
@@ -1,2 +1,3 @@
 
 from .concat_module import ConcatGatModule, ConcatCgcnModule
+from .bert_module import BertModule
--- gatbert/stance_classifier.py
+++ gatbert/stance_classifier.py
@@ -24,48 +24,6 @@ class StanceClassifier(torch.nn.Module):
     def get_grads(self):
         return []
 
-class BertClassifier(StanceClassifier):
-    def __init__(self,
-                 pretrained_model: str = DEFAULT_MODEL,
-                 predictor_bias: bool = False):
-        super().__init__()
-
-        label2id = {s.name:s.value for s in Stance}
-        id2label = {v:k for k,v in label2id.items()}
-        self.config = BertConfig.from_pretrained(pretrained_model, id2label=id2label, label2id=label2id)
-        self.bert = BertForSequenceClassification.from_pretrained(pretrained_model, config=self.config)
-        self.tokenizer: BertTokenizerFast = BertTokenizerFast.from_pretrained(pretrained_model)
-
-        if not predictor_bias:
-            # The bert config doesn't let us enable or disable the bias, so we just set it to 0
-            # and never let it get updated during training
-            pred_bias = self.bert.classifier.bias
-            pred_bias.data[:] = 0.
-            pred_bias.requires_grad = False
-
-        self.__encoder = self.Encoder(self.tokenizer)
-
-    def get_encoder(self):
-        return self.__encoder
-
-    def forward(self, *args, **kwargs):
-        bert_output = self.bert(*args, **kwargs)
-        return bert_output.logits
-
-    class Encoder(Encoder):
-        def __init__(self, tokenizer: PreTrainedTokenizerFast):
-            self.__tokenizer = tokenizer
-        def encode(self, sample: Sample | PretokenizedSample):
-            return {
-                **encode_text(self.__tokenizer, sample),
-                'stance': torch.tensor([sample.stance.value])
-            }
-        def collate(self, samples: List[TensorDict]) -> TensorDict:
-            return {
-                **collate_ids(self.__tokenizer, samples, return_attention_mask=True),
-                'stance': keyed_scalar_stack(samples, 'stance')
-            }
-
 
 class HybridClassifier(StanceClassifier):
     def __init__(self,
