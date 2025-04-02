c15e1b906bfd8cbb3b2eb9b83813e3963f27ba2c
Ethan L. Mines <elmines@users.noreply.github.com>
2025-04-02 16:29:15 -0400

    Update luo preprocess script

--- utils/cat_luo_cndata.sh
+++ utils/cat_luo_cndata.sh
@@ -2,5 +2,5 @@
 
 echo "If you're reading this and the script hasn't terminated, you probably meant to run './cat_luo_cn_data.sh < conceptnet_english.txt'" >&2
 
-cut -f3,1,2 /dev/stdin | awk '{printf "DUMMY\t%s\t%s\t%s\n", $3, $1, $2}'
+cut -f3,1,2 /dev/stdin | awk '{printf "DUMMY\t/r/%s\t/c/en/%s\t/c/en/%s\n", $3, $1, $2}'
 
