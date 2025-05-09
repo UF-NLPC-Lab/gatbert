efb87ff00962fc99ef1ef5b7a1c1facd34b50f46
Ethan L. Mines <elmines@users.noreply.github.com>
2025-05-09 14:35:41 -0400

    Update gridsearch code

--- stance_gator/domain_param_search.py
+++ stance_gator/domain_param_search.py
@@ -1,4 +1,5 @@
 # STL
+import json
 import os
 import argparse
 from itertools import product, chain
@@ -12,6 +13,15 @@ from torch.utils.data import DataLoader
 from .data import parse_ez_stance, MapDataset
 from .constants import EzstanceDomains, DEFAULT_BATCH_SIZE
 from .adv_module import AdvModule
+
+def grid_iter(**params):
+    names = []
+    domains = []
+    for (name, domain) in params.items():
+        names.append(name)
+        domains.append(domain)
+    for hvals in product(*domains):
+        yield dict(zip(names, hvals))
 
 def main(raw_args=None):
     parser = argparse.ArgumentParser()
@@ -41,31 +51,45 @@ def main(raw_args=None):
             train_samples.append(s)
 
 
+    best_score = -1
+    best_config = {}
     os.makedirs(args.out, exist_ok=True)
-    adv_weights   = [0.25, 0.75]
-    recon_weights = [1e-1, 1]
-    reg_weights   = [1e-1, 1, 1e1]
-    for (adv_weight, recon_weight, reg_weight) in product(adv_weights, recon_weights, reg_weights):
+    for hparam_dict in grid_iter(adv_weight=[0.25, 0.75], recon_weight=[1e-1, 1], reg_weight=[1e-1, 1, 1e1]):
+        print(f"Testing config {hparam_dict}")
         seed_everything(0)
 
         # FIXME: Really inefficient to re-encode these samples each time
-        module = AdvModule(held_out=heldout,
-                           adv_weight=adv_weight,
-                           recon_weight=recon_weight,
-                           reg_weight=reg_weight)
+        module = AdvModule(held_out=heldout, **hparam_dict)
         encode = module.encoder.encode
         collate = module.encoder.collate
         train_loader = DataLoader(MapDataset(map(encode, train_samples)), batch_size=DEFAULT_BATCH_SIZE, collate_fn=collate)
         val_loader = DataLoader(MapDataset(map(encode, val_samples)), batch_size=DEFAULT_BATCH_SIZE, collate_fn=collate)
 
+        version = "_".join([f"{name}_{val}" for name,val in hparam_dict.items()])
         logger = CSVLogger(save_dir=args.out,
                            name=f"heldout_{heldout}",
-                           version=f"adv_{adv_weight}_rec_{recon_weight}_reg_{reg_weight}",)
-        trainer = L.Trainer(max_epochs=1, log_every_n_steps=10, enable_checkpointing=False, logger=logger)
+                           version=version)
+        trainer = L.Trainer(
+                            # max_epochs=1,
+                            max_steps=4,
+                            log_every_n_steps=1,
+                            enable_checkpointing=False,
+                            logger=logger
+                            )
         trainer.fit(model=module, train_dataloaders=train_loader)
-        trainer.test(model=module, dataloaders=val_loader)
-
+        [test_scores] = trainer.test(model=module, dataloaders=val_loader)
+        with open(os.path.join(logger.log_dir, 'hparams.json'), 'w') as w:
+            w.write(json.dumps(hparam_dict))
+        macro_f1 = test_scores['test_macro_f1']
+        if macro_f1 > best_score:
+            print("New best!")
+            best_score = macro_f1
+            best_config = hparam_dict
         break
+    print(f"Best config: {best_config}")
+    with open(os.path.join(logger.root_dir, 'best.json'), 'w') as w:
+        out_json = {'config': best_config, 'test_macro_f1': best_score}
+        w.write(json.dumps(out_json))
 
 
 
