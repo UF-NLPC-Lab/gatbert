aae13f8dc46b29bc1ef2c456c5b79d26e2bc566e
Ethan L. Mines <elmines@users.noreply.github.com>
2025-05-13 15:50:29 -0400

    Update fit_test defaults

--- stance_gator/cli.py
+++ stance_gator/cli.py
@@ -22,7 +22,7 @@ def cli_main(**cli_kwargs):
     )
     early_stopping_callback = EarlyStopping(
         monitor=STOPPING_METRIC,
-        patience=10,
+        patience=3,
         mode='max'
     )
 
@@ -34,7 +34,8 @@ def cli_main(**cli_kwargs):
             "callbacks": [
                 model_callback,
                 early_stopping_callback
-            ]
+            ],
+            "deterministic": True
         },
         **cli_kwargs
     )
