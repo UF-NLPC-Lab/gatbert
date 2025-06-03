e16804b735ebffe73b4db3c1fabefb130427ed5a
Ethan L. Mines <elmines@users.noreply.github.com>
2025-06-03 18:25:24 -0400

    Load checkpoints in run_viz

--- stance_gator/run_viz.py
+++ stance_gator/run_viz.py
@@ -1,8 +1,8 @@
 import typing
 import pathlib
 # 3rd Party
+import torch
 from lightning.pytorch.cli import LightningArgumentParser
-from lightning.pytorch.callbacks import Callback
 # Local
 from .cli import StanceCLI
 from .base_module import StanceModule
@@ -11,6 +11,7 @@ from .data_modules import VizDataModule
 class VizCLI(StanceCLI):
     def add_arguments_to_parser(self, parser: LightningArgumentParser):
         super().add_arguments_to_parser(parser)
+        parser.add_argument("--ckpt", type=pathlib.Path, metavar="model.ckpt", required=True, help="Model checkpoint")
         parser.add_argument("-o", type=pathlib.Path, metavar="output/", required=True, help="Output directory")
 
 def main(raw_args=None):
@@ -25,6 +26,7 @@ def main(raw_args=None):
     )
 
     model = typing.cast(StanceModule, cli.model)
+    model.load_state_dict(torch.load(cli.config.ckpt)['state_dict'])
     datamodule = typing.cast(VizDataModule, cli.datamodule)
 
     out_dir = typing.cast(pathlib.Path, cli.config.o)
